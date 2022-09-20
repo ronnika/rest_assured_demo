fcb6f8d5eb0f9248d1a47ffc57fcd1dd5c2206ae
ronnika <lunkestr@gmail.com>
2022-09-20 20:54:58 +0300

    add hasSize and hasItem tests

---
+++ src/test/java/io/zippopotam/Chapters1and2Tests.java
@@ -0,0 +1,77 @@
+package io.zippopotam;
+
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
+public class Chapters1and2Tests {
+
+    @Test
+    public void requestZipCodeExpectBeverlyHills() {
+
+        given().
+        when().
+                get("http://api.zippopotam.us/us/90210").
+        then().
+            assertThat().
+                body("places[0].'place name'", equalTo("Beverly Hills"));
+    }
+
+    @Test
+    public void requestZipCodeHasItem_expectBeverlyHills() {
+
+        given().
+                when().
+                get("http://api.zippopotam.us/us/90210").
+                then().
+                assertThat().
+                body("places.'place name'", hasItem("Beverly Hills"));
+    }
+
+    @Test
+    public void requestZipCodeHasSize_expectNumberOfPlaceNames() {
+
+        given().
+                when().
+                get("http://api.zippopotam.us/us/90210").
+                then().
+                assertThat().
+                body("places.'place name'", hasSize(1));
+    }
+
+    @Test
+    public void requestZipCodeExpect200() {
+
+        given().
+                when().
+                get("http://api.zippopotam.us/us/90210").
+                then().
+                assertThat().
+                statusCode(200);
+    }
+
+    @Test
+    public void requestZipCodeContentType_expectJson() {
+
+        given().
+                when().
+                get("http://api.zippopotam.us/us/90210").
+                then().
+                assertThat().
+                contentType(ContentType.JSON);
+    }
+
+    @Test
+    public void requestZipCode_logResponseAndRequest() {
+
+        given().
+                log().all().
+                when().
+                get("http://api.zippopotam.us/us/90210").
+                then().
+                log().body();
+    }
+
+}
