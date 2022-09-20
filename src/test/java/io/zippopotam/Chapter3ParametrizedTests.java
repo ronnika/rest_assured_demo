0ab7d4a00a965b3bafb240542ea9bcfe99077ea1
ronnika <lunkestr@gmail.com>
2022-09-20 20:55:13 +0300

    add parameterized test

---
+++ src/test/java/io/zippopotam/Chapter3ParametrizedTests.java
@@ -0,0 +1,28 @@
+package io.zippopotam;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.*;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class Chapter3ParametrizedTests {
+
+    @CsvSource(value = {
+            "us,90210,Beverly Hills",
+            "us,90601,Whittier",
+            "ca,B2R,Waverley",
+    })
+    @ParameterizedTest(name = "For country and zip expected place")
+    public void requestZipCode_expectPlaceName(String country, String zipCode, String placeName) {
+        given().
+                pathParam("country", country).
+                pathParam("zipCode", zipCode).
+        when().
+                get("http://api.zippopotam.us/{country}/{zipCode}").
+        then().
+                assertThat().
+                body("places[0].'place name'", equalTo(placeName));
+    }
+}
